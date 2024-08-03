d30207111110ee495f56cce6daba9cbed4305067
Jans Rautenbach <git@moat.rauten.co.za>
2024-08-03 19:20:53 +0200

    better ical parser

--- src/routes/refresh-calendars/+server.ts
+++ src/routes/refresh-calendars/+server.ts
@@ -1,7 +1,23 @@
 import {db} from '$lib/server/db';
 import * as schema from '$lib/server/schema';
 import ical from 'node-ical';
+import ICAL from "ical.js";
 import {eq} from 'drizzle-orm'
+
+function getEventDetails(ev: [any]){
+	function getDeet(ev: [any], property: string): string{
+		const filtered = ev.filter(deet => deet[0] === property);
+		if (filtered.length === 0){
+			throw new Error(`Could not find property ${property} in event!`);
+		}
+		return filtered[0][3];
+	}
+
+	const summary = getDeet(ev, 'summary');
+	const start = new Date(getDeet(ev, 'dtstart'));
+	const end = new Date(getDeet(ev, 'dtend'));
+	return {summary, start, end};
+}
 
 export async function POST(){
 	const remoteIcss = await db.query.remoteIcs.findMany();
@@ -20,37 +36,35 @@ export async function POST(){
 			headers,
 		});
 
-		// https://www.npmjs.com/package/node-ical
-		const parsed = ical.parseICS(await resp.text());
+		// https://github.com/kewisch/ical.js
+		const parsed = ICAL.parse(await resp.text());
+		const allEvents = parsed[2];
 
+		// Define time limits
 		const startLimit = new Date();
 		startLimit.setDate(startLimit.getDate()-7);
 		const endLimit = new Date();
 		endLimit.setDate(endLimit.getDate()+14);
 
 		const calData = [];
-		for (const ev of Object.values(parsed)){
-				if (ev.type !== 'VEVENT') continue;
+		for (const ev of allEvents) {
+			if (ev[0] !== 'vevent') continue;
 
-				// recurring event
-				if (ev.rrule){
-					const durMins = Math.round((ev.end.getTime() - ev.start.getTime())/(1000*60));
-					const dates = ev.rrule.between(startLimit, endLimit, true);
-					// ignoring tz issues for now
-					for (const startDate of dates){
-						const endDate = new Date(startDate);
-						endDate.setMinutes(endDate.getMinutes()+durMins);
-						calData.push({
-							summary: ev.summary,
-							start: startDate,
-							end: endDate,
-						});
-					}
+			let cleanEvent;
+			try {
+				cleanEvent = getEventDetails(ev[1]);
+			}catch(err){
+				console.log('Failed to get event details! Skipping...');
+				console.log(err);
+				console.log('Event data:')
+				console.log(ev)
+				continue
+			}
 
-				}else if (ev.end >= startLimit && ev.start <= endLimit)
-						calData.push({summary: ev.summary, start: ev.start, end: ev.end});
-				}
-
+			if (cleanEvent.end >= startLimit && cleanEvent.start <= endLimit) {
+				calData.push(cleanEvent);
+			}
+		}
 
 		// Clear the old events
 		console.log('Clearing events with ICS_id='+remoteIcs.id);
