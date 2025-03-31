d807b738c6cd0c020c2c7ffe8db15b5527428cc3
Jans Rautenbach <gitlab@moat.rauten.co.za>
2025-03-31 12:44:52 +0000

    ci-buildx

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: [
+			'5173-j4nsr-calendarconcierg-uekxomdmxgl.ws-eu118.gitpod.io'
+		]
+	}
 });
